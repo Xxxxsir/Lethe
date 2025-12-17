f0ec83481ff212161042b7e964eb437eeb4dbf48
Justin Gao <xxxxsir@icloud.com>
2025-12-17 15:46:52 +0800

    update

--- cleanmodel_train.py
+++ cleanmodel_train.py
@@ -392,7 +392,6 @@ def get_accelerate_model(args, checkpoint_dir):
         use_fast=False, # Fast tokenizer giving issues.
         tokenizer_type='llama' if 'llama' in args.model_name_or_path else None, # Needed for HF name change
         trust_remote_code=args.trust_remote_code,
-        use_auth_token='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',
     )
     if tokenizer._pad_token is None:
         smart_tokenizer_and_embedding_resize(
